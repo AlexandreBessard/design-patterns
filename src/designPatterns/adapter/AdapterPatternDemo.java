4907b5dcc1b220e66fe8e43ae0b81d172ebdc444
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-25 23:48:00 +0200

    AdapterPatternDemo test

--- src/designPatterns/adapter/AdapterPatternDemo.java
+++ src/designPatterns/adapter/AdapterPatternDemo.java
@@ -10,6 +10,7 @@ public class AdapterPatternDemo {
     public static void main(String[] args) {
 
         AudioPlayer audioPlayer = new AudioPlayer();
+
         audioPlayer.play("mp3", "beyond the horizon.mp3");
 
         audioPlayer.play("mp4", "alone.mp4");
